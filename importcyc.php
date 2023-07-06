da956ec10c2490926af482545c54bf96d72deb63
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-07-06 13:24:13 +0100

    importcyc.php

--- importcyc.php
+++ importcyc.php
@@ -5,25 +5,81 @@
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
   <body>
-<?php 
+  <?php
 
 header('Content-Type: text/html; charset=UTF-8');
 header("Content-type: application/vnd.ms-excel"); 
 
-$cnx = mysqli_connect( "localhost", "root", "","basearab") ;
-$sql = "SELECT * FROM cycle"; 
-$result = mysqli_query($cnx, $sql);
-if (!$result) {
-    die('Error: ' . mysqli_error($cnx));
+$cnx = mysqli_connect("localhost", "root", "", "basearab");
+
+if (!$cnx) {
+    die('Failed to connect to the database: ' . mysqli_connect_error());
 }
-$nom_prenom = ''; // Variable to store the names from the database
-while ($row = mysqli_fetch_assoc($result)) {
-    $num_act .= $row['num_act'] . '_ ';
+
+if (isset($_POST['import'])) {
+    $theme = $_POST['theme'];
+    $num_salle = $_POST['num_salle'];
+    $date = $_POST['date_deb'];
+
+    $conditions = [];
+
+    if (!empty($theme)) {
+        $conditions[] = "theme = '$theme'";
+    }
+
+    if ($num_salle != 'رقم القاعة') {
+        $conditions[] = "num_salle = '$num_salle'";
+    }
+
+    if (!empty($date)) {
+        $conditions[] = "date_deb = '$date'";
+    }
+
+    $whereClause = implode(' AND ', $conditions);
+
+    $query = "SELECT * FROM cycle";
+
+    if (!empty($whereClause)) {
+        $query .= " WHERE " . $whereClause;
+    }
+
+    $result = mysqli_query($cnx, $query);
+
+    if (!$result) {
+        die('Error: ' . mysqli_error($cnx));
+    }
+
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+} else {
+    $query = "SELECT * FROM cycle";
+    $result = mysqli_query($cnx, $query);
+
+    if (!$result) {
+        die('Error: ' . mysqli_error($cnx));
+    }
+
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
 }
-$date = date(' d-m-Y');
-$filename = $num_act .' dans '. $date .'.xls';
+$themee = ''; 
+foreach ($data as $row) {
+    $themee .= $row['theme'] . '_';
+}
+
+$uniqueThemes = array_unique(array_column($data, 'theme'));
+$themes = implode('_', $uniqueThemes);
+
+$date = date('d-m-Y'); 
+
+$filename = $themes .' '. $date . '.xls';
+
 header("Content-Disposition: attachment; filename=$filename");
-$res_1 = mysqli_query($cnx,$sql);
+
  
 $tb1= " <table border='0' cellpadding='0' cellspacing='0'> 
 <tr bgcolor='#CCCCCC' height='40px'> 
@@ -37,17 +93,17 @@ $tb1= " <table border='0' cellpadding='0' cellspacing='0'>
 <td>قاعة عدد </td>
 
 </tr>";
-while ($data = mysqli_fetch_array($res_1)) 
+foreach ($data as $row) 
 { 
 $tb1 = $tb1 . "<tr>"; 
-$tb1 = $tb1 . "<td>" . $data['num_act'] . "</td>";
-$tb1 = $tb1 . "<td>" . $data['theme'] . "</td>"; 
-$tb1 = $tb1 . "<td>" . $data['date_deb'] . "</td>";
-$tb1 = $tb1 . "<td>" . $data['date_fin'] . "</td>"; 
-$tb1 = $tb1 . "<td>" . $data['for1'] . "</td>"; 
-$tb1 = $tb1 . "<td>" . $data['for1'] . "</td>"; 
-$tb1 = $tb1 . "<td>" . $data['for3'] . "</td>";
-$tb1 = $tb1 . "<td>" . $data['num_salle'] . "</td>"; 
+$tb1 = $tb1 . "<td>" . $row['num_act'] . "</td>";
+$tb1 = $tb1 . "<td>" . $row['theme'] . "</td>"; 
+$tb1 = $tb1 . "<td>" . $row['date_deb'] . "</td>";
+$tb1 = $tb1 . "<td>" . $row['date_fin'] . "</td>"; 
+$tb1 = $tb1 . "<td>" . $row['for1'] . "</td>"; 
+$tb1 = $tb1 . "<td>" . $row['for1'] . "</td>"; 
+$tb1 = $tb1 . "<td>" . $row['for3'] . "</td>";
+$tb1 = $tb1 . "<td>" . $row['num_salle'] . "</td>"; 
 
 $tb1 = $tb1 . "</tr>"; 
 } 
