85737e52646de55c28587bfd12c3454271d64b34
Rblaze23 <117004011+Rblaze23@users.noreply.github.com>
2023-07-06 13:23:48 +0100

    importexcel.php

--- importexcel.php
+++ importexcel.php
@@ -1,140 +1,142 @@
-
-<?php 
+<?php
 
 header('Content-Type: text/html; charset=UTF-8');
 header("Content-type: application/vnd.ms-excel"); 
 
 $cnx = mysqli_connect("localhost", "root", "", "basearab");
-$sql = "SELECT * FROM participant";
 
-$result = mysqli_query($cnx, $sql);
-if (!$result) {
-    die('Error: ' . mysqli_error($cnx));
+if (!$cnx) {
+    die('Failed to connect to the database: ' . mysqli_connect_error());
 }
 
-$nom_prenom = ''; // Variable to store the names from the database
-while ($row = mysqli_fetch_assoc($result)) {
-    $nom_prenom .= $row['nom_prenom'] . '_';
+if (isset($_POST['import'])) {
+    $nom_prenom = $_POST['nom_prenom'];
+    $num_salle = $_POST['num_salle'];
+    $date = $_POST['date'];
+
+    $conditions = [];
+
+    if (!empty($nom_prenom)) {
+        $conditions[] = "nom_prenom = '$nom_prenom'";
+    }
+
+    if ($num_salle != 'رقم القاعة') {
+        $conditions[] = "num_salle = '$num_salle'";
+    }
+
+    if (!empty($date)) {
+        $conditions[] = "date_debut = '$date'";
+    }
+
+    $whereClause = implode(' AND ', $conditions);
+
+    $query = "SELECT * FROM participant";
+
+    if (!empty($whereClause)) {
+        $query .= " WHERE " . $whereClause;
+    }
+
+    $result = mysqli_query($cnx, $query);
+
+    if (!$result) {
+        die('Error: ' . mysqli_error($cnx));
+    }
+
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+} else {
+    $query = "SELECT * FROM participant";
+    $result = mysqli_query($cnx, $query);
+
+    if (!$result) {
+        die('Error: ' . mysqli_error($cnx));
+    }
+
+    $data = [];
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
 }
 
-$date = date('d-m-Y'); // Current date
+$themee = ''; 
+foreach ($data as $row) {
+    $themee .= $row['theme_part'] . '_';
+}
 
-$filename = $nom_prenom . $date . '.xls';
+$uniqueThemes = array_unique(array_column($data, 'theme_part'));
+$themes = implode('_', $uniqueThemes);
+
+$date = date('d-m-Y'); 
+
+$filename = $themes . $date . '.xls';
 header("Content-Disposition: attachment; filename=$filename");
 
-$res_1 = mysqli_query($cnx,$sql);
-$res_2 = mysqli_query($cnx,$sql);
-$res_3 = mysqli_query($cnx,$sql);
-$res_4 = mysqli_query($cnx,$sql); 
-$res_5 = mysqli_query($cnx,$sql); 
-$tb2= " <table border='0' cellpadding='0' cellspacing='0'> 
+$tb = "<table border='0' cellpadding='0' cellspacing='0'> 
 <tr bgcolor='#CCCCCC' height='40px'> 
 <td>رقم بطاقة التعريف الوطنية</td>
 <td>الإسم و اللقب</td> 
 <td>المؤسسة</td>
-<td>الهاتف القار </td>
-<td>الفاكس </td>
+<td>الهاتف القار</td>
+<td>الفاكس</td>
 <td>الهاتف المحمول</td>
-<td>البريد الإلكتروني </td>
+<td>البريد الإلكتروني</td>
 <td>الدورة التكوينية</td>
-<td>قاعة عدد </td>
+<td>قاعة عدد</td>
 <td>تاريخ  البداية</td> 
 </tr>";
-while ($data = mysqli_fetch_array($res_2)) 
-{ 
-$tb2 = $tb2 . "<tr>"; 
-$tb2 = $tb2 . "<td>" . $data['Cin'] . "</td>";
-$tb2 = $tb2 . "<td>" . $data['nom_prenom'] . "</td>"; 
-$tb2 = $tb2 . "<td>" . $data['entreprise'] . "</td>";
-$tb2 = $tb2 . "<td>" . $data['tel_fix'] . "</td>"; 
-$tb2 = $tb2 . "<td>" . $data['fax'] . "</td>"; 
-$tb2 = $tb2 . "<td>" . $data['tel_port'] . "</td>"; 
-$tb2 = $tb2 . "<td>" . $data['mail'] . "</td>";
-$tb2 = $tb2 . "<td>" . $data['theme_part'] . "</td>"; 
-$tb2 = $tb2 . "<td>" . $data['num_salle'] . "</td>"; 
-$tb2 = $tb2 . "<td>" . $data['date_debut'] . "</td>"; 
-$tb2 = $tb2 . "</tr>"; 
-} 
-$tb2 .="</table>"; 
 
-print $tb2 ;
+foreach ($data as $row) {
+    $tb .= "<tr>"; 
+    $tb .= "<td>" . $row['Cin'] . "</td>";
+    $tb .= "<td>" . $row['nom_prenom'] . "</td>"; 
+    $tb .= "<td>" . $row['entreprise'] . "</td>";
+    $tb .= "<td>" . $row['tel_fix'] . "</td>"; 
+    $tb .= "<td>" . $row['fax'] . "</td>"; 
+    $tb .= "<td>" . $row['tel_port'] . "</td>"; 
+    $tb .= "<td>" . $row['mail'] . "</td>";
+    $tb .= "<td>" . $row['theme_part'] . "</td>"; 
+    $tb .= "<td>" . $row['num_salle'] . "</td>"; 
+    $tb .= "<td>" . $row['date_debut'] . "</td>"; 
+    $tb .= "</tr>"; 
+}
 
-$tb3= " <table border='0' cellpadding='0' cellspacing='0'> 
+$tb .= "</table>";
+
+print $tb;
+
+$tb2 = "<table border='0' cellpadding='0' cellspacing='0'> 
 <tr bgcolor='#CCCCCC' height='40px'> 
-<td>Cin</td>
-<td>Nom Prenom</td> 
-<td>Entreprise</td>
-<td>Tel_Fix</td>
-<td>Fax</td>
-<td>Tel_Port</td>
-<td>Email</td>
-<td>Theme_Part</td>
-<td>Num_Salle</td>
-<td>date de debut</td> 
-</tr>"; 
-while ($data = mysqli_fetch_array($res_3)) 
-{ 
-$tb3 = $tb3 . "<tr>"; 
-$tb3 = $tb3 . "<td>" . $data['Cin'] . "</td>";
-$tb3 = $tb3 . "<td>" . $data['nom_prenom_f'] . "</td>"; 
-$tb3 = $tb3 . "<td>" . $data['entreprise_f'] . "</td>";
-$tb3 = $tb3 . "<td>" . $data['tel_fix'] . "</td>"; 
-$tb3 = $tb3 . "<td>" . $data['fax'] . "</td>"; 
-$tb3 = $tb3 . "<td>" . $data['tel_port'] . "</td>"; 
-$tb3 = $tb3 . "<td>" . $data['mail'] . "</td>";
-$tb3 = $tb3 . "<td>" . $data['theme_part'] . "</td>"; 
-$tb3 = $tb3 . "<td>" . $data['num_salle'] . "</td>"; 
-$tb3 = $tb3 . "<td>" . $data['date_debut'] . "</td>"; 
-$tb3 = $tb3 . "</tr>"; 
-} 
-$tb3 .="</table>"; 
+<td>CIN</td>
+<td>Nom et Prenom</td> 
+<td>entreprise</td>
+<td>tel_fix</td>
+<td>fax</td>
+<td>tel_port</td>
+<td>email</td>
+<td>theme_part</td>
+<td>num_salle</td>
+<td>date_debut</td> 
+</tr>";
 
-print $tb3 ; 
+foreach ($data as $row) {
+    $tb2 .= "<tr>"; 
+    $tb2 .= "<td>" . $row['Cin'] . "</td>";
+    $tb2 .= "<td>" . $row['nom_prenom_f'] . "</td>"; 
+    $tb2 .= "<td>" . $row['entreprise_f'] . "</td>";
+    $tb2 .= "<td>" . $row['tel_fix'] . "</td>"; 
+    $tb2 .= "<td>" . $row['fax'] . "</td>"; 
+    $tb2 .= "<td>" . $row['tel_port'] . "</td>"; 
+    $tb2 .= "<td>" . $row['mail'] . "</td>";
+    $tb2 .= "<td>" . $row['theme_part'] . "</td>"; 
+    $tb2 .= "<td>" . $row['num_salle'] . "</td>"; 
+    $tb2 .= "<td>" . $row['date_debut'] . "</td>"; 
+    $tb2 .= "</tr>"; 
+}
 
-//$tb5= " <table border='0' cellpadding='0' cellspacing='0'> 
-//<tr bgcolor='#CCCCCC' height='40px'> 
-//<td>رقم بطاقة التعريف الوطنية</td>
-//<td>الإسم و اللقب</td> 
-//<td>المؤسسة</td>
-//<td>قاعة عدد </td>
-//<td>تاريخ  البداية</td> 
-//</tr>";
-//while ($data = mysqli_fetch_array($res_5)) 
-//{ 
-//$tb5 = $tb5 . "<tr>"; 
-//$tb5 = $tb5 . "<td>" . $data['Cin'] . "</td>";
-//$tb5 = $tb5 . "<td>" . $data['nom_prenom'] . "</td>"; 
-//$tb5 = $tb5 . "<td>" . $data['entreprise'] . "</td>";
-//$tb5 = $tb5 . "<td>" . $data['num_salle'] . "</td>"; 
-//$tb5 = $tb5 . "<td>" . $data['date_debut'] . "</td>"; 
-//$tb5 = $tb5 . "</tr>"; 
-//} 
-//$tb5 .="</table>"; 
+$tb2 .= "</table>";
 
-//print $tb5 ;
-
-//$tb4= " <table border='0' cellpadding='0' cellspacing='0'> 
-//<tr bgcolor='#CCCCCC' height='40px'> 
-//<td>Cin</td>
-//<td>Nom Prenom</td> 
-//<td>Entreprise</td>
-//<td>Num_Salle</td>
-//<td>date de debut</td> 
-//</tr>"; 
-//while ($data = mysqli_fetch_array($res_4)) 
-//{ 
-//$tb4 = $tb4 . "<tr>"; 
-//$tb4 = $tb4 . "<td>" . $data['Cin'] . "</td>";
-//$tb4 = $tb4 . "<td>" . $data['nom_prenom'] . "</td>"; 
-//$tb4 = $tb4 . "<td>" . $data['entreprise'] . "</td>";
-
-//$tb4 = $tb4 . "<td>" . $data['num_salle'] . "</td>"; 
-//$tb4 = $tb4 . "<td>" . $data['date_debut'] . "</td>"; 
-//$tb4 = $tb4 . "</tr>"; 
-//} 
-//$tb4 .="</table>"; 
-
-//print $tb4 ; 
-
-
+print $tb2;
+mysqli_close($cnx);
 ?>
